0f1c7de3556de3b33bc6ac9c631c3489ffd0dac6
deachawir-svg <deacha.wir@gmail.com>
2026-02-28 00:24:31 +0700

    update v1

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div> 
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/product_edit">product_edit</router-link>
+    </nav>
+    <router-view/>
+  </div> 
 </template>
 
 <style>
@@ -11,7 +14,7 @@
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  text-align: font; 
   color: #2c3e50;
 }
 
